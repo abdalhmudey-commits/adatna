e726e0050d839e260470d073166124ac9e6dcc84
عبد السلام <abdalhmudey@gmail.com>
2025-11-24 19:15:07 +0000

    A web manifest is a JSON text file that provides information about a web

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const repo = 'adatna'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
   output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
     unoptimized: true,
   },
