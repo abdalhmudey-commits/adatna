ed0df295e3f6982e79592594335ecea303218153
عبد السلام <abdalhmudey@gmail.com>
2025-11-24 15:17:38 +0000

    The icons in your web manifest must be fetchable and must have an image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ import SplashScreen from '@/components/splash-screen';
 const inter = Inter({ subsets: ['latin']});
 
 // This is required for GitHub Pages deployment. It must match the repository name.
-const basePath = process.env.NODE_ENV === 'production' ? '/adatna' : '';
+// const basePath = process.env.NODE_ENV === 'production' ? '/adatna' : '';
 
 
 // Since we are using 'use client', we can't export Metadata directly.
@@ -56,9 +56,9 @@ export default function RootLayout({
   return (
     <html lang="ar" dir="rtl" suppressHydrationWarning>
       <head>
-         <link rel="manifest" href={`${basePath}/manifest.json`} />
+         <link rel="manifest" href="/manifest.json" />
          <meta name="theme-color" content="#18453B" />
-         <link rel="apple-touch-icon" href={`${basePath}/icons/icon-192x192.png`}></link>
+         <link rel="apple-touch-icon" href="/icons/icon-192x192.png"></link>
          <meta name="apple-mobile-web-app-capable" content="yes" />
          <meta name="mobile-web-app-capable" content="yes" />
          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
