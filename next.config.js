0d1bce0a43de5ba6b8d54c0d6c0570336049768f
عبد السلام <abdalhmudey@gmail.com>
2025-11-24 19:21:41 +0000

    ما تفعلها انت الان هو شيء معيب بحق البرمجه اخبرني هل يمكننا كتابه الاكوا

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-const repo = 'adatna'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}`
-
 const nextConfig = {
   output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   images: {
     unoptimized: true,
   },
